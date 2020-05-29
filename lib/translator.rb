94449ff5a01b50ab4079d818c72e107b0da28847
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-29 22:49:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,31 @@
 # require modules here
-
-def load_library
-  # code goes here
+require 'pry'
+require 'yaml'
+def load_library(file)
+emoticons = YAML.load_file(file)
+dictionary ={}
+emoticons.each do |key, second_level|
+  dictionary[key] = {}
+  dictionary[key][:english] = second_level[0]
+  dictionary[key][:japanese] = second_level[1]
+end
+dictionary
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon()
   # code goes here
+  
 end
 
-def get_english_meaning
+def get_english_meaning(file, emoticon)
   # code goes here
+  found = []
+  dictionary = load_library(file)
+    dictionary.find do |key, second_level|
+      second_level.find do 
+        if emoticon == dictionary[key][:japanese]
+          return key
+        end
+      end
+    end
 end
