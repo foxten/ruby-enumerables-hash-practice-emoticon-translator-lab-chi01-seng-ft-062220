fead269e466793c89a5f9e9e142ac50bb34d2cdf
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-29 23:01:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,7 +25,9 @@ def get_english_meaning(file, emoticon)
       second_level.find do 
         if emoticon == dictionary[key][:japanese]
           return key
+        end
       end
+        "Sorry, that emoticon was not found"
     end
   end
 end
